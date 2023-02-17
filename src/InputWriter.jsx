97ac9105205d83940efe557496affb9e83060f11
Jean <jean.antoine.michelet@gmail.com>
2023-02-17 16:16:49 +0100

    InputWriter

--- src/InputWriter.jsx
+++ src/InputWriter.jsx
@@ -1,7 +1,14 @@
+import { useRef } from "react"
+
 export default function InputWriter() {
+    const containerRef = useRef(null)
+
+    function handleChange(e) {
+        containerRef.current.innerText = e.currentTarget.value
+    }
 
     return <>
-        <h2>Input write. Write here:<span></span></h2>
-        <input className="form-control" type="text" />
+        <h2>Write here: <span ref={containerRef}></span></h2>
+        <input onChange={handleChange} className="form-control" type="text" />
     </>
 }
