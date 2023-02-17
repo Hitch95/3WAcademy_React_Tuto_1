ad30eeedcaa8c82f603197c508cbecac9a2f9c9d
Jean <jean.antoine.michelet@gmail.com>
2023-02-17 12:11:07 +0100

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import Counter from './Counter';
 import Incrementer from './Incrementer';
 import StopWatch from './StopWatch';
 
 export default function App () {
+  const [colorTitle, setColorTitle] = useState(true)
+
+  function handleColor() {
+    setColorTitle(colorTitle => !colorTitle)
+  }
   return (
     <div className='container my-5'>
-      <h1>React tuto</h1>
-      {/* <Counter /> */}
-      {/* <Incrementer /> */}
+      <h1 className={colorTitle ? 'text-success' : ''}>React tuto</h1>
+        {colorTitle 
+        ?  <button className='btn btn-danger'
+              onClick={handleColor}>
+            Remove Color
+          </button>
+          : <button className='btn btn-success'
+              onClick={handleColor}>
+            Set Color
+          </button>}
+      <Counter />
+      <Incrementer />
       <StopWatch />
     </div>
   );
